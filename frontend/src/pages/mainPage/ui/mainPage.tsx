eb9fb09b075fc2ed89517a3c8f6efb016031198a
nickoff <nick.olehnovich@gmail.com>
2024-04-14 23:22:20 +0300

    feat: add font

--- frontend/src/pages/mainPage/ui/mainPage.tsx
+++ frontend/src/pages/mainPage/ui/mainPage.tsx
@@ -2,5 +2,9 @@ import clsx from 'clsx';
 import s from './mainPage.module.css';
 
 export const MainPage = () => {
-  return <div className={clsx(s.mainPage)}>MainPage</div>;
+  return (
+    <div className={clsx(s.mainPage)}>
+      <h3>Наши услуги</h3>
+    </div>
+  );
 };
