329c2bfab6df2717d16fd11f55ad55cb45db2cc3
nickoff <nick.olehnovich@gmail.com>
2024-04-29 21:28:31 +0300

    feat: refactor and new style dataWidget

--- frontend/src/shared/ui/dataWidget/dataWidget.tsx
+++ frontend/src/shared/ui/dataWidget/dataWidget.tsx
@@ -6,18 +6,22 @@ interface DataWidgetProps {
   title: string;
   icon: ReactElement;
   children: ReactNode;
+  className?: string;
+  variant?: 'filled';
 }
 
 export const DataWidget: FC<DataWidgetProps> = (props) => {
-  const { title, icon, children } = props;
+  const { title, icon, children, className, variant } = props;
 
   return (
-    <div className={clsx(s.dataWidget)}>
-      <div className={clsx(s.dataWidgetIcon)}>{icon}</div>
-      <div className={clsx(s.dataWidgetContent)}>
-        <div className={clsx(s.dataWidgetDataContainer)}>{children}</div>
-        <h5 className={clsx(s.dataWidgetTitle)}>{title}</h5>
+    <div className={clsx(s.dataWidget, className && className, variant === 'filled' && s.filled)}>
+      <div className={clsx(s.dataWidgetLayout)}>
+        <div className={clsx(s.dataWidgetIcon, variant === 'filled' && s.filled)}>{icon}</div>
+        <div className={clsx(s.dataWidgetContent)}>
+          <div className={clsx(s.dataWidgetContentData)}>{children}</div>
+        </div>
       </div>
+      <h5 className={clsx(s.dataWidgetTitle)}>{title}</h5>
     </div>
   );
 };
--- frontend/src/widgets/radioStationDataWidget/ui/radioStationDataWidget.tsx
+++ frontend/src/widgets/radioStationDataWidget/ui/radioStationDataWidget.tsx
@@ -7,96 +7,82 @@ import GenderIcon from 'shared/assets/icon/man-and-woman-svgrepo-com.svg?react';
 import AudienceAgeIcon from 'shared/assets/icon/time-management-svgrepo-com.svg?react';
 import { useAdSettingsStore } from 'shared/store';
 
+const DATAWIDGET_CONTENT_TEXT = {
+  BROADCAST_ZONE: 'зона вещания',
+  REACH_DLY_PEOPLE_DAY: 'охват аудитории (Reach Dly)',
+  REACH_DLY_PEOPLE_PRECENT: 'охват аудитории (Reach Dly)',
+  AUDIENCE_SEX: 'пол аудитории',
+  AUDIENCE_SEX_MEN: 'Мужчины',
+  AUDIENCE_SEX_FEMAL: 'Женщины',
+  AUDIENCE_AGE: 'возраст аудитории',
+  AUDIENCE_CORE: 'ядро аудитории',
+  UNIT_PEOPLE_DAY: ' чел./сутки',
+  UNIT_PRECENT: '%',
+  UNIT_UNDERFINED: '--'
+};
+
 export const RadioStationDataWidget = () => {
   const { selectedRadio } = useAdSettingsStore();
-
-  const data = [
-    {
-      title: 'охват аудитории (Reach Dly)',
-      icon: <ReachDlyIcon />,
-      value: [
-        <>
-          {selectedRadio?.reach_dly && (
-            <h4>
-              {selectedRadio?.reach_dly}
-              <span> чел./сутки</span>
-            </h4>
-          )}
-          {selectedRadio?.reach_dly_percent && (
-            <h4>
-              {selectedRadio?.reach_dly_percent}
-              <span> %</span>
-            </h4>
-          )}
-        </>
-      ]
-    },
-    {
-      title: 'зона вещания',
-      icon: <LocationIcon />,
-      value: [
-        <h4>
-          <br />
-          {selectedRadio?.broadcast_zone || '--'}
-        </h4>
-      ]
-    },
-    {
-      title: 'пол аудитории',
-      icon: <GenderIcon />,
-      value: [
-        <>
-          {!!selectedRadio?.audience_sex.length && (
-            <>
-              <h4>
-                <span>{selectedRadio?.audience_sex[1].sex || 'Женщины'} </span>
-                {selectedRadio?.audience_sex[1].percent || '--'}
-                <span>%</span>
-              </h4>
-              <h4>
-                <span>{selectedRadio?.audience_sex[0].sex || 'Мужчины'} </span>
-                {selectedRadio?.audience_sex[0].percent || '--'}
-                <span>%</span>
-              </h4>
-            </>
-          )}
-        </>
-      ]
-    },
-    {
-      title: 'возраст аудитории',
-      icon: <AudienceAgeIcon />,
-      value: [
-        <>
-          {!!selectedRadio?.audience_age.length && (
-            <>
-              <h4>
-                <span>ядро аудитории </span>
-                <br />
-                {selectedRadio?.audience_age[0].age || '--'}
-              </h4>
-              <h4>
-                {selectedRadio?.audience_age[0].percent || '--'}
-                <span> %</span>
-              </h4>
-            </>
-          )}
-        </>
-      ]
-    }
-  ];
 
   if (!selectedRadio) return null;
 
   return (
     <div className={clsx(s.radioStationDataWidget)}>
-      {data.map((item, index) => (
-        <DataWidget key={index} title={item.title} icon={item.icon}>
-          {item.value.map((text, index) => (
-            <div key={index}>{text}</div>
-          ))}
-        </DataWidget>
-      ))}
+      <DataWidget
+        className={clsx(s.DataWidget1)}
+        title={DATAWIDGET_CONTENT_TEXT.BROADCAST_ZONE}
+        icon={<LocationIcon />}
+        variant={'filled'}>
+        <h4>{selectedRadio?.broadcast_zone || DATAWIDGET_CONTENT_TEXT.UNIT_UNDERFINED}</h4>
+      </DataWidget>
+      <DataWidget
+        className={clsx(s.DataWidget2)}
+        title={DATAWIDGET_CONTENT_TEXT.REACH_DLY_PEOPLE_DAY}
+        icon={<ReachDlyIcon />}>
+        <h4>
+          {selectedRadio?.reach_dly
+            ? selectedRadio?.reach_dly.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
+            : DATAWIDGET_CONTENT_TEXT.UNIT_UNDERFINED}
+          <span>{DATAWIDGET_CONTENT_TEXT.UNIT_PEOPLE_DAY}</span>
+        </h4>
+      </DataWidget>
+      <DataWidget
+        className={clsx(s.DataWidget3)}
+        title={DATAWIDGET_CONTENT_TEXT.REACH_DLY_PEOPLE_PRECENT}
+        icon={<ReachDlyIcon />}>
+        <h4>
+          {selectedRadio?.reach_dly_percent
+            ? selectedRadio?.reach_dly_percent
+            : DATAWIDGET_CONTENT_TEXT.UNIT_UNDERFINED}
+          <span>{DATAWIDGET_CONTENT_TEXT.UNIT_PRECENT}</span>
+        </h4>
+      </DataWidget>
+      <DataWidget className={clsx(s.DataWidget4)} title={DATAWIDGET_CONTENT_TEXT.AUDIENCE_SEX} icon={<GenderIcon />}>
+        <h4>
+          <span>{selectedRadio?.audience_sex[1]?.sex || DATAWIDGET_CONTENT_TEXT.AUDIENCE_SEX_FEMAL} </span>
+          {selectedRadio?.audience_sex[1]?.percent || DATAWIDGET_CONTENT_TEXT.UNIT_UNDERFINED}
+          <span>{DATAWIDGET_CONTENT_TEXT.UNIT_PRECENT}</span>
+        </h4>
+        <h4>
+          <span>{selectedRadio?.audience_sex[0]?.sex || DATAWIDGET_CONTENT_TEXT.AUDIENCE_SEX_MEN} </span>
+          {selectedRadio?.audience_sex[0]?.percent || DATAWIDGET_CONTENT_TEXT.UNIT_UNDERFINED}
+          <span>{DATAWIDGET_CONTENT_TEXT.UNIT_PRECENT}</span>
+        </h4>
+      </DataWidget>
+      <DataWidget
+        className={clsx(s.DataWidget5)}
+        title={DATAWIDGET_CONTENT_TEXT.AUDIENCE_AGE}
+        icon={<AudienceAgeIcon />}>
+        <h4>
+          <span>{DATAWIDGET_CONTENT_TEXT.AUDIENCE_CORE}</span>
+          <br />
+          {selectedRadio?.audience_age[0]?.age || DATAWIDGET_CONTENT_TEXT.UNIT_UNDERFINED}
+        </h4>
+        <h4>
+          {selectedRadio?.audience_age[0]?.percent || DATAWIDGET_CONTENT_TEXT.UNIT_UNDERFINED}
+          <span>{DATAWIDGET_CONTENT_TEXT.UNIT_PRECENT}</span>
+        </h4>
+      </DataWidget>
     </div>
   );
 };
