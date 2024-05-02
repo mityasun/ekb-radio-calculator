09b75e227cbe85d02be037fd3d8dce288fb3b28d
Artem Petukhov <102475384+mityasun@users.noreply.github.com>
2024-05-02 15:26:14 +0500

    Change rest authentication
    Signed-off-by: Artem Petukhov <102475384+mityasun@users.noreply.github.com>

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -167,6 +167,11 @@ AUTH_USER_MODEL = 'users.User'
 # Rest settings
 
 REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
