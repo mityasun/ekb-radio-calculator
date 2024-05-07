fd2d945ede6d8ef3de7ab5e3c531c877c83e619a
nickoff <nick.olehnovich@gmail.com>
2024-05-07 14:58:44 +0300

    fix: fix adaptiv frame

--- frontend/src/app/layout/layout.tsx
+++ frontend/src/app/layout/layout.tsx
@@ -3,21 +3,10 @@ import s from './layout.module.css';
 import { Outlet } from 'react-router-dom';
 import { Header } from '../../widgets/header';
 import { Footer } from '../../widgets/footer';
-import { useEffect, useRef } from 'react';
 
 export const Layout = () => {
-  const layoutRef = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    if (layoutRef.current) {
-      const parentWindow = window.parent;
-      const message = layoutRef.current.scrollHeight;
-      parentWindow.postMessage(message, '*');
-    }
-  }, [layoutRef.current?.scrollHeight]);
-
   return (
-    <div className={clsx(s.layout)} ref={layoutRef}>
+    <div className={clsx(s.layout)}>
       <Header />
       <main className={clsx(s.layoutMain)}>
         <Outlet />
